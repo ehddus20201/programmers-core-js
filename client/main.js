72c3e1dd7d2cc13917be65db9ffbe1e45ffbb34a
ehddus20201 <ehddus20201@naver.com>
2025-06-18 09:49:46 +0900

    random dice

--- client/main.js
+++ client/main.js
@@ -1,78 +1,122 @@
-import { diceAnimation, getNode, attr, insertLast } from './lib/index.js';
+// const rollingButton = buttons[0];
+// const recordButton = buttons[1];
+// const resetButton = buttons[2];
 
-const [rollingButton, recordButton, resetButton] = document.querySelectorAll('.buttonGroup button');
+// setInterval(diceAnimation, 1000);
 
-const recordListWrapper = getNode('.recordListWrapper')
+/* 
+  1. 주사위 굴리기 버튼을 선택
+      - querySelector or getNode
+
+  2. 클릭 이벤트 바인딩
+      - click 
+
+  3. 버튼 클릭시 diceAnimation 애니메이션 동작
+      - setInterval diceAnimation
+
+  4. 같은 버튼을 눌렀을 때 토글 처리
+      - 상태 변수 만들기
+        - isClicked = false;
+
+        - isClicked = !isClicked;
+
+      - 조건 처리 
+
+  5. 애니메이션 재생 or 정지
+      - setInterval
+      - clearInterval 
+
+  6. recordButton, resetButton 활성화/비활성화
+      - target.disabled = true || false
+
+*/
+
+/* 
+
+  1. 주사위 눈 가져오기
+      - cube의 dice 속성 값 
+
+  2. 태그 만들고
+      - <tr>
+          <td>0</td> // 회사
+          <td>5</td> // 주사위 눈 수
+          <td>5</td> // 주사위 눈의 총 합
+        </tr>
+
+  3. 태그 렌더링하기
+      - insertLast
+
+*/
+
+import {
+  memo,
+  attr,
+  getNode,
+  endScroll,
+  insertLast,
+  clearContents,
+  diceAnimation,
+} from './lib/index.js';
+
+const [rollingButton, recordButton, resetButton] = document.querySelectorAll(
+  '.buttonGroup button'
+);
+const recordListWrapper = getNode('.recordListWrapper');
 
 
 let count = 0;
 let total = 0;
 
-
-function createItem(value){
+function createItem(value) {
   return `
-    tr>
+    <tr>
       <td>${++count}</td>
       <td>${value}</td>
-      <td>${total += value}</td>
+      <td>${(total += value)}</td>
     </tr>
-    `
-    
+  `;
 }
 
-function renderRecordItem(){
-  const dicenumber = +attr('#cube','dice');
-  console.log(dicenumber);
-  
-  
+function renderRecordItem() {
+  const diceNumber = +attr(memo('cube'), 'dice');
 
-
-
-    insertLast('tbody',createItem(dicenumber));
-    recordListWrapper.scrollTop = recordListWrapper.scrollHeight;
+  insertLast('tbody', createItem(diceNumber));
 }
 
-
-const handleRollingDice = (()=>{
-
+const handleRollingDice = (() => {
   let isClicked = false;
   let id;
 
-  return ()=>{
-  if(!isClicked){
-    id= setInterval( diceAnimation, 100);
-    recordButton.disabled = true;
-    resetButton.disabled = true;
-    
-  
-  }else{
-    clearInterval(id);
-    recordButton.disabled = false;
-    resetButton.disabled = false;
+  return () => {
+    if (!isClicked) {
+      id = setInterval(diceAnimation, 100);
+      recordButton.disabled = true;
+      resetButton.disabled = true;
+    } else {
+      clearInterval(id);
+      recordButton.disabled = false;
+      resetButton.disabled = false;
+    }
 
-    
-  }
-  isClicked = !isClicked;
+    isClicked = !isClicked;
   };
-})()
+})();
 
-function handleRecord(){
+
+function handleRecord() {
   recordListWrapper.hidden = false;
   renderRecordItem();
+  endScroll(recordListWrapper);
 }
 
-function handlereset() {
+
+function handleReset() {
   recordListWrapper.hidden = true;
-  
+  clearContents('tbody');
+  count = 0;
+  total = 0;
 }
 
-
-
-rollingButton.addEventListener('click',handleRollingDice);
-//1 주사위 ㅋ굴리기 버튼
-// 2 클릭 이벤트 바인딩
-// 셋 인터벌 함수로 diceAnimation 이 굴러가도록
-
-recordButton.addEventListener('click',handleRecord);
-resetButton.addEventListener('click',handlereset);
-
+rollingButton.addEventListener('click', handleRollingDice);
+recordButton.addEventListener('click', handleRecord);
+resetButton.addEventListener('click', handleReset);
